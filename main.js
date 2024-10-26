a70d05c889ef823c0837fc878e5dd53dcc16ad32
jubril <badmusjubril46@gmail.com>
2024-10-26 12:14:46 +0100

    js 3

---
+++ main.js
@@ -0,0 +1,138 @@
+// var classmate = ["chidinma","rosheed","eunice","jubril","mohammed","korede"]
+
+// console.log(classmate.length)
+
+// console.log(classmate[2])
+// classmate[0]='lee'
+
+// console.log(classmate)
+
+// classmate.push('fiyin')
+// console.log(classmate)
+
+// classmate.unshift('mubarak')
+// console.log(classmate)
+
+// var candidate = ['tpain']
+
+// if(candidate=== 'tpain' ){
+//     console.log('more pain for you')
+// }else{
+//     console.log('less pain ')
+// }
+
+// var inecAgeChecker =[undefined]
+
+// if(inecAgeChecker >= 70){
+//     console.log('please stay at home dont come and vote')
+// }else{
+//     console.log('you can vote')
+// }
+
+// var occupation = ['tech']
+
+// if(occupation === 'medicine'){
+//     console.log('you are a doctor')
+// }else if(occupation==='painter'){
+//     console.log('you are an artist')
+// }else if(occupation==='police'){
+//     console.log('you are an olopa')
+// }else{
+//     console.log('you are a tech bro')
+// }
+
+
+
+
+// var info = {
+//     name: 'jubril',
+//     mail: 'badmusjubril46@gmail.com',
+//     debt: null,
+//     age: 17,
+//     course: 'computer science',
+//     school: 'LASU',
+//     hobbies: ['coding', 'gaming'],
+//     address: {
+//         street: "YABA",
+//         city: "lagos",
+//         country: "nIGERIA"
+//     }
+
+
+
+// }
+
+// // function declaration
+// function greeting(){
+//     console.log('Hello world')
+// }
+
+
+// // function excecution
+
+// greeting()
+
+
+// function sum(){
+//     console.log(5+5)
+// }
+
+// sum()
+
+
+// function profit(cp,sp){
+//     var gain = sp-cp
+//     console.log(gain)
+// }
+// profit(5000,11000)
+// profit(15000,33000)
+
+// function loss(sp,cp){
+//     var lose = cp-sp
+//     console.log(lose)
+// }
+
+// loss(5000,30000)
+
+
+function area(L){
+    var area = L**2
+    console.log(area)
+}
+area(5
+
+   
+)
+
+function circle(pie,r){
+    var circle = pie*r**2
+    console.log(circle)
+}
+
+circle(3.14,7)
+
+
+function percent(cp,sp){
+    var percent = (sp-cp)/cp * 100
+    console.log(percent)
+}
+
+percent(3000,5000)
+
+function sum(a,b){
+    var sum = a+b
+    console.log(sum)
+    
+}
+sum(4,2)
+
+
+
+function celcius(c){
+    var celcius = (c*1.8)+32
+    console.log(celcius)
+}
+
+celcius(10)
+
+
